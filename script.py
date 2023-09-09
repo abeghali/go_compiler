913425b8ba8ca2da270193cad97b4ae880164d38
Abanoub Ghali <abeghali19@gmail.com>
2023-09-08 23:40:18 -0500

    Upload script with latest progress made.

--- script.py
+++ script.py
@@ -15,10 +15,6 @@ GAME_DIR_PATTERN = "game"
 
 def main():
     
-    cwd = os.getcwd()
-    source_path = os.path.join(cwd, source)
-    target_path = os.path.join(cwd, target)
-
     args = sys.argv
 
     # Raise an exception if the script doesn't receive the 2 required arguments when run, source
@@ -29,10 +25,51 @@ def main():
     # Only assigning the values beginning at index 1 to disregard the name of the script itselt.
     source, target = args[1:]
 
+    # Get current working directory, to join to path that was passed as an argument.
+    cwd = os.getcwd()
+    source_dir = os.path.join(cwd, source)
+    target_dir = os.path.join(cwd, target)
+
+    game_paths = find_all_game_paths(source_dir)
+
+    # For testing.
+    print(game_paths)
+    print(len(game_paths))
+
+    create_dir(target_dir)
+
 #=====================================================================================================================
 
 def find_all_game_paths(source):
 
+    game_paths = []
+
+    # To "walk" recursively through the passed source path.
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        break
+    
+    return game_paths
+
+#=====================================================================================================================
+
+# A simple function that checks if the target directory exists or not, and if it doesn't, then it creates it.
+def create_dir(target):
+
+    if not os.path.exists(target):
+        os.mkdir(target)
+
+#=====================================================================================================================
+
+def get_name_from_paths(paths, to_rmv):
+
+    new_names = []
+
+    for path in paths:
+        -, dir_name = os.path.split(path)
 
 #=====================================================================================================================
 
